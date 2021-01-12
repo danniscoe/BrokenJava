e87a7329f73ff340a1c38e2e274f35b799ee354a
Danni St.Claire-Coe <47538480+danniscoe@users.noreply.github.com>
2021-01-11 23:12:51 -0600

    COmmit new Main.java
    Deleted original file to include something more simple.

---
+++ Main.java
@@ -0,0 +1,15 @@
+package com.TTS;
+
+class AddIntegers {
+
+    public static void main(String[] args) {
+
+        int first = 56;
+        int second = 210;
+
+        System.out.println("Enter two numbers: " + first + " " + second);
+        int sum = first + second;
+
+        System.out.println("The sum of these two numbers is: " + sum);
+    }
+}
